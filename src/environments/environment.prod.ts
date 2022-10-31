60539df13db2f65dfe9103789f43fb5959945c25
Sudip modi <sudipinshunya@gmail.com>
2022-10-31 19:15:07 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: 'http:127.0.0.1:8000',
+  base_url: 'http:127.0.0.1:8000/api',
   game_stage_url: "https://staging.slotegrator.com/api/index.php/v1"
 };
